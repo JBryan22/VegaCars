c8ad988300e35bd58c6a5782f684182689f600ba
jbryan22 <jesse.bryan22@gmail.com>
2018-02-22 14:03:35 -0800

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vega_New
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //this is where we can register services for dependency injections such as
+            //services.AddTransient<IRepository, EFRepository> -- this creates an instance of EF whenever the IRepository is in the constructor of a method (such as in a controller)
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //look into logger factory in console logs aren't showing up
+
+            //this is where we customize our middleware -- everything below this is middleware
+
+            //to change environment at system level go to ControlPanel > System > Advanced Settings -- create a new user varialble names ASPNETCORE_ENVIRONMENT wit the value set to Development
+            //to change env at terminal level type "$Env:ASPNETCORE_ENVIRONMENT="Development""
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    //allows us to not have to manually rebuild everytime we change something in angular.
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
